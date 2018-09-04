ee9faff52994ef0dfe05e8a105a460fa613ab4ea
Roz King <roz@rkingdc.com>
2018-09-04 03:59:15 +0000

    update model specification

--- regplot/code/copy_imgs.py
+++ regplot/code/copy_imgs.py
@@ -1,6 +1,7 @@
 import os
 import random
 from shutil import copy
+from functools import partial
 from multiprocessing import Pool
 
 os.chdir(os.path.expanduser('~/share/rkingdc-blog/regplot'))
@@ -25,18 +26,18 @@ def indx12(samp, n1, n2):
     samp2 = [s for i,s in enumerate(samp) if i in idx0]
     return samp1, samp2
 
-def cpytest(x, source_dir='data/png'):
-    copy(os.path.join(source_dir, x), 'data/imgs/test')
-def cpytrain(x, source_dir='data/png'):
-    copy(os.path.join(source_dir, x), 'data/imgs/train')
+def cpyfile(x, dest=None, source_dir='data/png'):
+    copy(os.path.join(source_dir, x), dest)
     
-if __name__ == '__main__':
 
-    train_none, test_none = indx12(none, n_train, n_test)
-    train_any, test_any = indx12(any_error, n_train, n_test)
+train_none, test_none = indx12(none, n_train, n_test)
+train_any, test_any = indx12(any_error, n_train, n_test)
+
+with Pool(3) as p:
+    print("Copying Training controls...")
+    p.map(partial(cpyfile,dest=os.path.join(train_dir, 'none')), train_none)
+    p.map(partial(cpyfile,dest=os.path.join(train_dir, 'any_error')), train_any)  
     
-    with Pool(3) as p:
-        print("Copying Training set...")
-        p.map(cpytrain,train_none+train_any)
-        print("Copying test set...")
-        p.map(cpytest, test_none+test_any)
+    print("Copying test set...")
+    p.map(partial(cpyfile,dest=os.path.join(test_dir, 'none')), test_none)
+    p.map(partial(cpyfile,dest=os.path.join(test_dir, 'any_error')), test_any)     
